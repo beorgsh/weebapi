5b30b8f0db6a6e7c115e90c08713c10849bf5f8b
beorgsh <jeorgeseniagan11@gmail.com>
2025-11-21 22:46:31 +0800

    Add files via upload

---
+++ weebAPI-main/weebAPI/utils/episodetemplate.py
@@ -0,0 +1,11 @@
+class Episode:
+    def __init__(self, id, animeid, epNumber, snapshot, lang, duration, episodeLink):
+        self.id = id 
+        self.animeid = animeid 
+        self.epNumber = epNumber
+        self.snapshot = snapshot
+        self.lang=lang
+        self.duration=duration
+        self.episodeLink = episodeLink
+    
+
